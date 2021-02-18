02b0dd9fd554aa57f49f8301666cd50b2d024698
camila bernales <camilabernales09@gmail.com>
2021-02-17 23:48:45 -0300

    fix - fix object in Bitlogico.js

--- src/components/profile/Bitlogico.js
+++ src/components/profile/Bitlogico.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react'
-import Profile from "./Profile"
-import imageTest from "../../imageTest/emmawatson.jpg"
+import React, { Component } from "react";
+import Profile from "./Profile";
+import imageTest from "../../imageTest/emmawatson.jpg";
 export default class Bitlogico extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            name: "",
-            lastname:"",
-            image:{imageTest},
-            phoneNumber:""
-        }
-    }
-    render() {
-        return (
-            <div>
-             <Profile bitlogico = {this.state}/>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      bitlogico: {
+        name: "",
+        lastname: "",
+        image: { imageTest },
+        phoneNumber: "",
+      },
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Profile bitlogico={this.state.bitlogico} />
+      </div>
+    );
+  }
 }
